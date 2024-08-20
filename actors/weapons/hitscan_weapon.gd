6570e2f65fc567ed09633ae35e36e8bd7f364972
vib <vibrunazo@gmail.com>
2024-08-19 21:51:45 -0300

    78. Recoil and Raycasts

--- actors/weapons/hitscan_weapon.gd
+++ actors/weapons/hitscan_weapon.gd
@@ -1,23 +1,25 @@
 extends Node3D
 
-## How many times per second the weapon can fire
-@export var fire_rate: float = 14.0
+## How many times per second the weapon can shoot
+@export var shoot_rate: float = 14.0
+## How far to recoil in meters
+@export var recoil: float = 0.05
+## The mesh that will be animated during recoil
+@export var weapon_mesh: Node3D
 
-
+@onready var ray_cast: RayCast3D = $RayCast
 @onready var cooldown_timer: Timer = $CooldownTimer
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+@onready var weapon_position: Vector3 = weapon_mesh.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire"):
 		if cooldown_timer.is_stopped():
-			cooldown_timer.start(1.0 / fire_rate)
-			fire()
+			shoot()
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10)
 
-func fire() -> void:
-	print('fired')
+func shoot() -> void:
+	cooldown_timer.start(1.0 / shoot_rate)
+	print('shootd at %s ' % [ray_cast.get_collider()])
+	weapon_mesh.position.z += recoil
