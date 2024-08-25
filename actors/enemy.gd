8fe827b682e4211ffbd3d402ff09edb5d4432021
vib <vibrunazo@gmail.com>
2024-08-25 20:53:19 -0300

    96. Making the Level Playable

--- actors/enemy.gd
+++ actors/enemy.gd
@@ -2,9 +2,9 @@ class_name Enemy
 extends CharacterBody3D
 
 
-const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@export var speed := 8.0
 @export var max_hitpoints: float = 100
 @export var aggro_range: float = 12.0
 @export var attack_range: float = 1.5
@@ -30,10 +30,11 @@ var is_linking: bool = false
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 	setup_nav()
-	animation_player.play("RESET") # was not resetting after level restart
+	animation_player.play("idle") # was not resetting after level restart
 	
 
 #func _process(delta: float) -> void:
+	#print(animation_player.current_animation)
 	#if provoked:
 	#navigation_agent.target_position = player.global_position
 
@@ -48,7 +49,6 @@ func update_path() -> void:
 	if is_linking: return
 	if provoked:
 		navigation_agent.target_position = player.global_position
-	
 
 func _physics_process(delta: float) -> void:
 	next_position = navigation_agent.get_next_path_position()
@@ -62,11 +62,11 @@ func _physics_process(delta: float) -> void:
 	
 	if direction:
 		look_at_target(direction)
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
 	try_attack()
@@ -97,3 +97,6 @@ func _on_navigation_agent_3d_link_reached(details: Dictionary) -> void:
 	is_linking = true
 	await get_tree().create_timer(2).timeout
 	is_linking = false
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	animation_player.play("idle")
